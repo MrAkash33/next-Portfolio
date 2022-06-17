c759e4e4306a61c5beb3fb87bec797d6ebdcfaa5
Fyndora <79826594+MrAkash33@users.noreply.github.com>
2022-06-18 01:44:33 +0530

    Work On Header

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import { Container, Grid } from "@mui/material";
-
+import Image from 'next/image'
+import MyImage from '../public/mrpatsy1.png'
 export default function Home() {
   return (
     <>  
-      <Container>
+      <div>
       <Grid container spacing={2}>
-      <Grid item xs={6}>
+      <Grid item xs={7}>
         <div className="title">
           <h1>Hy! I Am <br></br>Akash</h1>
         </div>
       </Grid>
-      <Grid item xs={6}>
-      <h2>Hii</h2>
+      <Grid item xs={5}>
+      <Image src={MyImage}   layout="responsive" />
       </Grid>
       </Grid>
-      </Container>
+      </div>
     </>
   )
 }
