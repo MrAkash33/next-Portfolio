c833f496977ff2c0ca198237e5aed5b88664845d
Fyndora <79826594+MrAkash33@users.noreply.github.com>
2022-06-18 19:24:26 +0530

    set Background

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,25 @@ import Image from 'next/image'
 import MyImage from '../public/mrpatsy1.png'
 export default function Home() {
   return (
-    <>  
-      <div>
-      <Grid container spacing={2}>
-      <Grid item xs={7}>
-        <div className="title">
-          <h1>Hy! I Am <br></br>Akash</h1>
-        </div>
-      </Grid>
-      <Grid item xs={5}>
-      <Image src={MyImage}   layout="responsive" />
-      </Grid>
-      </Grid>
-      </div>
+    <>
+      <section>
+        <Grid container spacing={2}>
+          <Grid item xs={8} style={{ margin: 'auto' }} className="sideWrapper">
+            <div className="title">
+              <Container>
+                <h1>Hy ! I Am <br></br> <span style={{color:'#fca61f'}}>Akash</span></h1>
+                <h3 style={{ textAlign: 'center' }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br></br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <br></br> when an unknown printer took a galley of type and scrambled it to make a <br></br> type specimen book.  It has survived not only five centuries.</h3>
+              </Container>
+            </div>
+          </Grid>
+          <Grid item xs={4} className="mypic">
+            <Image src={MyImage} layout="responsive" />
+          </Grid>
+        </Grid>
+      </section>
+      <section>
+
+      </section>
     </>
   )
 }
