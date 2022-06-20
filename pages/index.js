2f9adbd5d39b1da908e7fefa0f7c2d2d75624e60
Fyndora <79826594+MrAkash33@users.noreply.github.com>
2022-06-20 22:38:25 +0530

    style Change
    Update Style

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Card } from "@mui/material";
 import Image from 'next/image'
 import MyImage from '../public/mrpatsy1.png'
 export default function Home() {
@@ -11,6 +11,13 @@ export default function Home() {
               <Container>
                 <h1>Hy! I Am <br></br> <span style={{ color: '#fca61f' }}>Akash</span></h1>
                 <h3 style={{ textAlign: 'center' }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
+                <div className="" style={{ textAlign: 'center' }}>
+                  <li>
+                    <div class="wrapper">
+                      <a href="#" class="fancy-button1 bg-gradient2"><span><i class="fa fa-wheelchair-alt"></i>Hire me</span></a>
+                    </div>
+                  </li>
+                </div>
               </Container>
             </div>
           </Grid>
@@ -26,7 +33,44 @@ export default function Home() {
         </Grid>
       </section>
       <section>
-
+        <Container>
+          <Grid container style={{ marginTop: '50px' }}>
+            <Grid item xs={5} style={{margin:'auto',position:'relative'}}>
+              <div className="myawesomeBx">
+                <h2>My awesome</h2>
+                <h1>services</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+              </div>
+            </Grid>
+            <Grid item xs={7}>
+              <Container>
+                <Grid container>
+                  <Grid item xs={6} className="DesignBx1" >
+                    <div className="DesignBx">
+                      <h1>Design</h1>
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                      <button>Learn More1</button>
+                    </div>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <div className="DesignBx">
+                      <h1>Design</h1>
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                      <button>Learn More2</button>
+                    </div>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <div className="DesignBx">
+                      <h1>Design</h1>
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                      <button>Learn More3</button>
+                    </div>
+                  </Grid>
+                </Grid>
+              </Container>
+            </Grid>
+          </Grid>
+        </Container>
       </section>
     </>
   )
